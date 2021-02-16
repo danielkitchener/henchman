f2489f604314b4f9e196ed14551742501d88cb73
Daniel Kitchener <danielki@live.com>
2021-02-15 20:39:35 -0500

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HenchmanForm from './HenchmanForm';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Henchman for Killer Sudoku';
+  });
   return (
     <div className="App">
+      <title>Henchman for Killer Sudoku</title>
       <header className="App-header">Henchman for Killer Sudoku</header>
       <HenchmanForm />
     </div>
